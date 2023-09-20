b911edc0d6719f0d46dc96f6b46d859eb1846b7e
Riley Meere <rime1945@colorado.edu>
2023-09-20 05:37:38 -0600

    javascript to abbrev. nav titles on mobile devices
    this is literally the smoothest scaling you have ever seen

--- ClassPortal/script.js
+++ ClassPortal/script.js
@@ -1,1 +1,19 @@
 console.log("This is working");
+
+// source: https://css-tricks.com/working-with-javascript-media-queries/
+// seems legit
+function checkMediaQuery() {
+    // If the inner width of the window is less than 400px
+    if (window.innerWidth < 400) {
+        // Then abbreviate these titles
+        document.body.querySelector("#assignments").innerHTML = "A";
+        document.body.querySelector("#projects").innerHTML = "P";
+    } else {
+        document.body.querySelector("#assignments").innerHTML = "Assignments";
+        document.body.querySelector("#projects").innerHTML = "Projects"; 
+    }
+  }
+  
+  // Add a listener for when the window resizes
+  window.addEventListener('resize', checkMediaQuery);
+//console.log(x);
