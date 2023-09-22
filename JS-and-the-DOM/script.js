c9064f9f7c634c692b6335efa6982504744eb45f
Riley Meere <rime1945@colorado.edu>
2023-09-21 23:14:44 -0600

    struggling

--- JS-and-the-DOM/script.js
+++ JS-and-the-DOM/script.js
@@ -130,10 +130,3 @@ function heartAnimation(e) {
 document.addEventListener('click', (e) => {
     heartAnimation(e);
 })
-
-$('.no-zoom').bind('touchend', function(e) {
-    e.preventDefault();
-    // Add your code here. 
-    $(this).click();
-    // This line still calls the standard click event, in case the user needs to interact with the element that is being clicked on, but still avoids zooming in cases of double clicking.
-  })
